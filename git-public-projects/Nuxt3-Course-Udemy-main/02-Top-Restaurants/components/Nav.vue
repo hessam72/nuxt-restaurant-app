9dfa04912b920c336be524c4e3c4b1c505f20c7a
hessam72 <hessamtahani@gmail.com>
2024-08-09 16:21:55 +0330

    adding layout and meta

---
+++ git-public-projects/Nuxt3-Course-Udemy-main/02-Top-Restaurants/components/Nav.vue
@@ -0,0 +1,31 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container-fluid">
+      <NuxtLink class="navbar-brand" to="/">Restaurantly</NuxtLink>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav ml-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <NuxtLink
+              to="/"
+              class="nav-link"
+              activeClass="active"
+              aria-current="page"
+              >Home</NuxtLink
+            >
+          </li>
+          <li class="nav-item">
+            <NuxtLink to="/restaurants" class="nav-link" activeClass="active"
+              >Restaurants</NuxtLink
+            >
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<style scoped>
+.ml-auto {
+  margin-left: auto;
+}
+</style>
